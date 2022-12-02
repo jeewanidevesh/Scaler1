56c5ecdd7d10f7b5cb0ff81403a1217d38b4e6cc
DEVESH <deveshjeewani@gmail.com>
2022-12-02 19:22:57 +0530

    LLD practice

---
+++ src/designpatterns/singleton/doublecheckloading/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package designpatterns.singleton.doublecheckloading;
+
+public class DatabaseConnection {
+    private static DatabaseConnection databaseConnection=null;
+
+    private DatabaseConnection(String name){
+
+    }
+
+    public static DatabaseConnection getInstance(String name){
+        if(databaseConnection==null){
+            synchronized (DatabaseConnection.class){
+                if(databaseConnection==null){
+                    databaseConnection=new DatabaseConnection(name);
+                }
+            }
+        }
+
+        return databaseConnection;
+    }
+}
