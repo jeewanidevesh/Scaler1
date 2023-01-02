0340b6ecb7b5c7721611b2b92e2b4dbd1b19867e
DEVESH <deveshjeewani@gmail.com>
2023-01-02 12:48:16 +0530

    LLD practice

---
+++ src/designpatterns/strategy/BikePathCalculatorStrategy.java
@@ -0,0 +1,8 @@
+package designpatterns.strategy;
+
+public class BikePathCalculatorStrategy  implements PathCalculatorStrategy{
+    @Override
+    public void findPath(String from, String to) {
+
+    }
+}
---
+++ src/designpatterns/strategy/CarPathCalculatorStrategy.java
@@ -0,0 +1,8 @@
+package designpatterns.strategy;
+
+public class CarPathCalculatorStrategy implements PathCalculatorStrategy{
+    @Override
+    public void findPath(String from, String to) {
+
+    }
+}
